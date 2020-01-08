b10474343e24d208ad3f81722188cae147c3fb32
chrisp-dev <chris.paradeise@gmail.com>
2020-01-08 11:30:17 -0800

    Added scoob page and reservation form html

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -6,10 +7,23 @@ const PORT = process.env.PORT || 3000;
 // app.use
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use('/assets', express.static(path.join(__dirname, "assets", "styles")));
+app.use('/images', express.static(path.join(__dirname, "assets", "images")));
+app.use(express.static(path.join(__dirname, "views")));
 // app.get
 app.get('/', function (req, res) {
     res.json("Welcome to the API!");
 });
+
+// app.get('/api/scoob', (req, res) => {
+//     res.sendFile(path.join(__dirname, "views", "scoob.html"));
+// });
+
+// app.get('/api/graf', (req, res) => {
+//     res.sendFile(path.join(__dirname, "assets", "images", "scoob.jpg"));
+// });
+
 // app.listen
 app.listen(PORT, function () {
     console.log("Listening on port " + PORT);
