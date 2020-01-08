c8be2c4b88aba2f7f6749e98d02e09effc7038fc
chrisp-dev <chris.paradeise@gmail.com>
2020-01-08 13:14:53 -0800

    Added Tables.json and writing/reading from file

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const util = require('util');
 const Table = require('./lib/Table');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const readFileAsync = util.promisify(fs.readFile);
+
 const TableList = [];
+
+readFileAsync(path.join(__dirname, "Tables.json"))
+    .then(data => {
+        if (data.length)
+            TableList.push(...JSON.parse(data));
+    }).catch(err => {
+        console.log(err);
+    });
 
 // app.use
 app.use(express.urlencoded({ extended: true }));
@@ -28,9 +40,14 @@ app.get('/api/waitlist/:id', (req, res) => {
 });
 
 app.post('/api/reserve', (req, res) => {
-    console.log(req.body);
-    TableList.push(new Table(req.body));
-    res.json(req.body);
+    // console.log(req.body);
+    const newTable = new Table(req.body);
+    console.log(newTable);
+    TableList.push(newTable);
+    fs.writeFile("Tables.json", JSON.stringify(TableList), (err) => {
+        console.log('file written');
+        res.json(newTable);
+    });
 });
 
 
@@ -38,19 +55,19 @@ var tables = [];
 
 var waitlist = [];
 
-app.post("/api/tables", function(req, res) {
+app.post("/api/tables", function (req, res) {
 
     var newReservation = req.body;
-  
+
     console.log(newReservation);
-  
+
     tables.push(newReservation);
-  
+
     res.json(newReservation);
 });
 
 
- // app.listen
+// app.listen
 app.listen(PORT, function () {
     console.log("Listening on port " + PORT);
 });
